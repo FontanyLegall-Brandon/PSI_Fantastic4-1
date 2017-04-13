90b38a8d2af7bfb7f357ef9e5c51829cbb43d0eb
callerisa corto <cc504980@tamaris.unice.fr>
2017-04-13 16:31:52 +0200

    Ajout avg-2.pdf : moyenne de nos 3 mesures

--- Mesures2/Avg/script.py
+++ Mesures2/Avg/script.py
@@ -1,25 +1,29 @@
-ants_far = open('ants-far.csv', 'r', encoding='utf-8')
-ants_middle = open('ants-middle.csv', 'r', encoding='utf-8')
-ants_near = open('ants-near.csv', 'r', encoding='utf-8')
+ants_far = open('ants-far2.csv', 'r', encoding='utf-8')
+ants_middle = open('ants-middle2.csv', 'r', encoding='utf-8')
+ants_near = open('ants-near2.csv', 'r', encoding='utf-8')
 ants_avg = open('ants-avg.csv', 'w', encoding='utf-8')
 
 af = ants_far.readline()
+af = af.split("\t")
 am = ants_middle.readline()
+am = am.split("\t")
 an = ants_near.readline()
-i = 1
-while len(af) != 0 :
+an = an.split("\t")
+
+
+while af != [''] :
     count = 0
-    if af[1] == '\t' :
-        count = int(af[2:]) + int(am[2:]) + int(an[3:])
-    else :
-        count = int(af[3:]) + int(am[3:]) + int(an[3:])
-    ants_avg.write('{}\t'.format(i)+str(count/3)+'\n')    
+    print("af : ",af)
+    count = int(af[1]) + int(am[1]) + int(an[1])
+    ants_avg.write('{}\t'.format(af[0])+str(count/3)+'\n')    
     af = ants_far.readline()
+    af = af.split("\t")
     am = ants_middle.readline()
+    am = am.split("\t")
     an = ants_near.readline()
-    if i == 1 : i -= 1
-    i += 5
-ants_avg.write('{}\t'.format(i)+str(count/3)+'\n')    
+    an = an.split("\t")
+
+#ants_avg.write('{}\t'.format(af[)+str(count/3)+'\n')    
 ants_far.close()    
 ants_middle.close()
 ants_near.close()
